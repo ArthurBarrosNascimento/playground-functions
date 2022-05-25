73532018e6f5d3ee67481edc4c392a23ecf66f27
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-05-24 23:07:49 -0300

    80% do projeto

--- src/challenges.js
+++ src/challenges.js
@@ -68,17 +68,61 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(arryNumeros) {
   // seu código aqui
+  let result = [];
+
+  for (let i = 0; i < arryNumeros.length; i += 1) {
+   
+    if (arryNumeros[i] % 3 === 0 && arryNumeros[i] % 5 === 0) {
+      result.push("fizzBuzz");
+
+    }
+    else if (arryNumeros[i] % 3 === 0) {
+      result.push("fizz");
+    }
+    else if (arryNumeros[i] % 5 === 0) {
+      result.push("buzz");
+    }
+    else {
+      result.push("bug!");
+    }
+  }
+  return result;
 }
 
 // Desafio 9
-function encode() {
+function encode(mensagemEncode) {
   // seu código aqui
+  let regexA = /"a"/gim;
+  let regexE = /"e"/gim;
+  let regexI = /"i"/gim;
+  let regexO = /"o"/gim;
+  let regexU = /"u"/gim;
+  let result = mensagemEncode;
+  for (let i = 0; i < mensagemEncode.length; i += 0) {
+    if (mensagemEncode === "a") {
+      result.replace(regexA, "1");
+    }
+    else if (mensagemEncode === "e") {
+      result.replace(regexE, "2");
+    }
+    else if (mensagemEncode === "i") {
+      result.replace(regexI, "3");
+    }
+    else if (mensagemEncode === "o") {
+      result.replace(regexO, "4");
+    }
+    else if (mensagemEncode === "u") {
+      result.replace(regexU, "5");
+    }
+  }
+  return result;
 }
 function decode() {
   // seu código aqui
-}
+  
+} 
 
 // Desafio 10
 function techList() {
